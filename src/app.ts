9d719ce829a7ac95c8448c67130c84b79df8bdae
Hari Krishna Mada <harikrishna.mada@colruytgroup.com>
2018-03-30 12:54:16 +0200

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,62 @@
-console.log('Hello TypeScript!');
+const pizzas = [
+    {
+        name : 'Pepporini',
+        toppings: ['pepporini']
+    },
+    {
+        name : 'Dominos',
+        toppings: ['pepporini']
+    },
+    {
+        name : 'PizzaHut Pizzas',
+        toppings: ['pepporini']
+    }
+    
+];
+
+const pizzas2 = [
+    {
+        name : 'Pepporini-2',
+        toppings: ['pepporini-2']
+    },
+    {
+        name : 'Dominos-2',
+        toppings: ['pepporini']
+    },
+    {
+        name : 'PizzaHut Pizzas-2',
+        toppings: ['pepporini']
+    }
+    
+];
+
+const mappedPizzas = pizzas.map(pizza =>
+     pizza.name.toUpperCase()
+);
+
+const mappedPizzas1 = pizzas.map(function(pizza){
+return pizza.name.toUpperCase();
+}
+    
+);
+
+
+
+console.log(mappedPizzas);
+
+
+
+const pizza = {
+    name: 'Blazzing Inferno',
+    getName: function(){
+        setTimeout(() => {
+            console.log(this.name);
+        },1000);
+    }
+};
+
+console.log(pizza.getName());
+
+
+
+
