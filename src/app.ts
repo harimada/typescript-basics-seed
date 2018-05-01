b2c5d1b6128433ecf40b955f4a3f1ca99fce120a
Hari Krishna Mada <harikrishna.mada@colruytgroup.com>
2018-05-01 10:28:52 +0200

    functional methods map, filter, reduce

--- src/app.ts
+++ src/app.ts
@@ -99,13 +99,13 @@ function multiply(a: number, b: number = 25) {
 
 // }
 
-// const toppings = ['bacon', 'chilli'];
+ const toppings = ['bacon', 'chilli','pepperoni'];
 
-// const newToppings =  ['pepperoni'];
+ const newToppings =  ['pepperoni','chilli'];
 
-// const allToppings = [...toppings, ...newToppings];
+ const allToppings = [...toppings, ...newToppings];
 
-// console.log(allToppings);
+ //console.log(allToppings);
 
 // const pizza = {
 //   name: "Pepperoni",
@@ -134,4 +134,84 @@ function multiply(a: number, b: number = 25) {
 // logToppings(toppings);
 
 
-parseInt('15',10);
+//parseInt('15',10);
+
+const first = {
+  name: 'hari',
+  role: 'SE',
+  company:'colruyt'
+};
+
+const last = {
+  name: 'krishna',
+  role: 'SWE'
+}
+
+const first_last = {...first, ...last};
+//console.log(first_last);
+
+const myString: string = 'hari';
+const n: number = +myString; // convert to number
+const b: boolean = !!myString; // convert to boolean
+//console.log('value !! ---> ', b);
+
+export interface Test{
+  name: string,
+  duration: number;
+}
+
+const tasks = [
+   {
+     'name'     : 'Write for Envato Tuts+',
+     'duration' : 120
+   },
+ 
+  {
+     'name'     : 'Work out',
+     'duration' : 60
+ 
+  },
+ 
+  {
+     'name'     : 'Procrastinate on Duolingo',
+     'duration' : 240
+   }
+ ];
+
+
+
+ const task_names_f = tasks.map((task, index, array) =>
+                                                     task.name.toLowerCase());
+
+
+//console.log(task_names_f);
+
+ const task_names = tasks.map(task => {
+    
+  return {'name' : task.name.toUpperCase(), 'duration' : task.duration}
+}).filter(task => 
+  task.duration >= 120 );
+
+console.log(task_names);
+
+
+
+function greeting(name: string) {
+  //console.log("name---->",name);
+}
+
+function processUserInput(callback:Function) {
+  //var name = prompt('Please enter your name.');
+  callback("hari");
+}
+
+processUserInput(greeting);
+
+//greeting("hari");;
+
+// Using arrow functions
+
+const numbers = [1, 2, 3, 4, 5];
+var total = numbers.reduce((previous, current, index) => previous + current );
+
+console.log("The loop is done, and the final value is " + total + ".");
